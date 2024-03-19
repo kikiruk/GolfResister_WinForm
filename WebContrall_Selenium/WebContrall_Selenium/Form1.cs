bcb7979f7dc1c290ed490867ce04b9bbcf74b226
seagull <33773761+kikiruk@users.noreply.github.com>
2024-03-20 00:26:05 +0900

    모바일 주소에서 PC 주소로 변경
    모바일 주소를 줘서 PC 주소로 올바르게 변경,
    문제가 생길 줄 알았으나 정상 작동됨

--- WebContrall_Selenium/WebContrall_Selenium/Form1.cs
+++ WebContrall_Selenium/WebContrall_Selenium/Form1.cs
@@ -29,7 +29,7 @@ namespace WebContrall_Selenium
             //자바스크립트 요소를 통해서 요소가 생성되기 전에도 클릭가능
             IJavaScriptExecutor jsExecutor = (IJavaScriptExecutor)driver;
 
-            driver.Navigate().GoToUrl("https://m.elysian.co.kr/member/login.asp");
+            driver.Navigate().GoToUrl("https://www.elysian.co.kr/member/login.asp");
 
             // 아이디 입력 필드 찾기 ,아이디 입력
             jsExecutor.ExecuteScript("document.getElementById('userinfo01_01').value = arguments[0];", "11037301");
@@ -52,7 +52,7 @@ namespace WebContrall_Selenium
         private void button1_Click(object sender, EventArgs e)
         {
             LoginState();
-            driver.Navigate().GoToUrl("https://m.elysian.co.kr/reservation/golf_choice.asp");
+            driver.Navigate().GoToUrl("https://www.elysian.co.kr/reservation/golf.asp");
         }
     }
 }
