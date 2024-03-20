9a43ed11398f8f133f88d07ab4807f65bd72b198
seagull <kikiruk123@naver.com>
2024-03-21 01:53:33 +0900

    로그인 한뒤 날자 선택하고 시간 선택하는것 까지 구현
    1. 날자 선택은 달이 넘어가면 선택이 안되는 부분 수정해야함
    2. 대기중인 상태에서 가장 빠르게 접속하여서
    부킹 하는것을 어떻게 구현할지 생각해봐야함
    3. 로그인을 미리 해서 대기를 하고 부킹을 하는부분은 따로 구현해서 따로 실행하게끔 구현해야함

--- WebContrall_Selenium/WebContrall_Selenium/Form1.Designer.cs
+++ WebContrall_Selenium/WebContrall_Selenium/Form1.Designer.cs
@@ -29,24 +29,35 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            dateLable = new Label();
             button1 = new Button();
+            dateTimePicker1 = new DateTimePicker();
             SuspendLayout();
             // 
             // label1
             // 
-            label1.AutoSize = true;
-            label1.Font = new Font("맑은 고딕", 21.75F, FontStyle.Regular, GraphicsUnit.Point, 129);
-            label1.Location = new Point(1, -1);
+            label1.Font = new Font("맑은 고딕", 13F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            label1.Location = new Point(12, 9);
             label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(613, 60);
+            label1.Size = new Size(784, 80);
             label1.TabIndex = 0;
-            label1.Text = "로그인 후 원하는 페이지 이동";
+            label1.Text = "날자 : 선택 날자로 고정\r\n시간 : 선택 시간 이후 시간대중 가장 가까운시간으로 예약됨";
             label1.Click += label1_Click;
+            // 
+            // dateLable
+            // 
+            dateLable.AutoSize = true;
+            dateLable.Location = new Point(330, 110);
+            dateLable.Name = "dateLable";
+            dateLable.Size = new Size(219, 32);
+            dateLable.TabIndex = 3;
+            dateLable.Text = "날자 및 시간 선택 :";
+            dateLable.Click += dateLable_Click;
             // 
             // button1
             // 
-            button1.Location = new Point(12, 112);
+            button1.Location = new Point(12, 110);
             button1.Name = "button1";
             button1.Size = new Size(286, 125);
             button1.TabIndex = 1;
@@ -54,12 +65,24 @@
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.CustomFormat = "yyyy-MM-dd HH:mm";
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.Location = new Point(566, 110);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.ShowUpDown = true;
+            dateTimePicker1.Size = new Size(230, 39);
+            dateTimePicker1.TabIndex = 2;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(14F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImageLayout = ImageLayout.Center;
             ClientSize = new Size(825, 446);
+            Controls.Add(dateLable);
+            Controls.Add(dateTimePicker1);
             Controls.Add(button1);
             Controls.Add(label1);
             Font = new Font("맑은 고딕", 12F, FontStyle.Regular, GraphicsUnit.Point, 129);
@@ -74,6 +97,8 @@
         #endregion
 
         private Label label1;
+        private Label dateLable;
         private Button button1;
+        private DateTimePicker dateTimePicker1;
     }
 }
--- WebContrall_Selenium/WebContrall_Selenium/Form1.cs
+++ WebContrall_Selenium/WebContrall_Selenium/Form1.cs
@@ -1,32 +1,40 @@
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
 using OpenQA.Selenium.DevTools.V120.FedCm;
+using OpenQA.Selenium.Support.UI;
+using System.Collections.ObjectModel;
+using static System.Windows.Forms.LinkLabel;
+using System.CodeDom.Compiler;
+using System.Windows.Forms;
 
 namespace WebContrall_Selenium
 {
     public partial class Form1 : Form
     {
-        //chrome 브라우저와 소동 하게 해주는 ChromeDriver
-        private IWebDriver driver;
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
-            
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
         }
 
         private void LoginState()
         {
-            driver = new ChromeDriver();
+            //chrome 브라우저와 소동 하게 해주는 ChromeDriver
+            IWebDriver driver = new ChromeDriver();
             //자바스크립트 요소를 통해서 요소가 생성되기 전에도 클릭가능
+
+            // `IJavaScriptExecutor` 인터페이스를 사용하여 Selenium WebDriver에서 JavaScript 코드를 실행할 수 있습니다.
+            // 이 인터페이스는 테스트 자동화 중 웹 페이지의 자바스크립트 환경에 직접 접근하여 코드를 실행하게 해줍니다.
+            // 예를 들어, 페이지에 보이지 않는 요소를 클릭하거나, 페이지의 DOM을 직접 조작하는 등의 작업을 할 수 있습니다.
+            // 이를 통해, 테스트 시나리오에서 요구하는 복잡한 웹 상호작용을 구현할 수 있습니다.
             IJavaScriptExecutor jsExecutor = (IJavaScriptExecutor)driver;
 
             driver.Navigate().GoToUrl("https://www.elysian.co.kr/member/login.asp");
@@ -40,19 +48,56 @@ namespace WebContrall_Selenium
 
             jsExecutor.ExecuteScript("fnLogin(1);");
 
-            //// 아이디 저장 체크박스 찾기
-            //IWebElement SaveIdCheckbox = driver.FindElement(By.Id("id_save"));
-            //SaveIdCheckbox.Click();
-            //
-            //// 자동로그인 체크박스 찾기
-            //IWebElement LoggedInAutoCheckbox = driver.FindElement(By.Id("login_auto"));
-            //LoggedInAutoCheckbox.Click();
+            //로그인 기능 외 구현 추후 수정.
+            driver.Navigate().GoToUrl("https://www.elysian.co.kr/reservation/golf.asp");
+
+
+            //웹이 반응을 하는 상태까지 기다리기 : 최대기다리는 초 10초
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until(wd => jsExecutor.ExecuteScript("return document.readyState").ToString() == "complete");
+
+            //부킹 목적 날자에서 날자 (예시로 2024-03-24 형태) 만 추출함
+            String purposeYearMonthDay = dateTimePicker1.Value.ToString().Substring(0, 10);
+
+            //태그 예시
+            //<a href="javascript:void(0);" title="예약가능" onclick="fnChoiceDate(this, '2024-03-24', 'POSS');" class="golfResvDate poss">24</a>
+            //POSS 는 possible 이라는 뜻이었음 날자가 지나면 'END' 예약이 다 된 날짜는 'IMPOSS'
+            //XPath 의 contains 문법을 사용하여서 목적 날자와 (ex '2024-03-24' 식의형태) 일치하는 태그 찾기
+            IWebElement YearMonthDayTag = driver.FindElement(By.XPath("//a[contains(@onclick, '" + purposeYearMonthDay + "')]"));
+
+            //함수를 HTML에 나와있는 이름 그대로 하면 제대로 동작 안하고 이렇게 정확하게
+            //XPath 로 해당 태그에 접근해서 onclick 의 함수를 호출해야동작함
+            jsExecutor.ExecuteScript("arguments[0].onclick()", YearMonthDayTag);
+
+            //태그 예시
+            //< a href = "javascript:void(0);" onclick = "fnChoiceCourseTime(this, 'A', 'VALLEY', '0632');" class="timeBtn golfResvCourseTime">06:32</a>
+            //'fnChoiceCourseTime(this,' 가 onclick 에 포함되어있는 a 태그를 찾는다 
+            IReadOnlyCollection<IWebElement> HourMinuteTagsCollection = driver.FindElements(By.XPath("//a[contains(@onclick, 'fnChoiceCourseTime(this,')]"));
+
+            foreach (var HourMinuteTag in HourMinuteTagsCollection)
+            {
+                string onClickValue = HourMinuteTag.GetAttribute("onclick");
+                
+                //onClickVaue 는 onclick 시 호출되는자바스크립트 함수를 문자열로 나타낸것이다.
+                //숫자부분은 항상 끝에서 7번째 에서부터 4글자이므로 이를 이용한다.
+                int startPoint = onClickValue.Length - 7;
+                int purposeHourMinute = dateTimePicker1.Value.Hour * 100 + dateTimePicker1.Value.Minute;
+                if (int.Parse(onClickValue.Substring(startPoint, 4)) >= purposeHourMinute)
+                {
+                    jsExecutor.ExecuteScript("arguments[0].onclick()", HourMinuteTag);
+                    break;
+                }
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             LoginState();
-            driver.Navigate().GoToUrl("https://www.elysian.co.kr/reservation/golf.asp");
+        }
+
+        private void dateLable_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
