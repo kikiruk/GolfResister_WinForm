1780280503d9fd783f4fe26f75a9b3747fe7a83c
seagull <kikiruk123@naver.com>
2024-03-22 19:34:52 +0900

    코드 정리
    함수의 역할을 LoginAndGoToReservationAndReady 와 GoToReservationAndSelectMonthDateTime 로 구분하여 , 대기중인 상태와 등록을 시도하는 상태로 구분함

--- WebContrall_Selenium/WebContrall_Selenium/Form1.cs
+++ WebContrall_Selenium/WebContrall_Selenium/Form1.cs
@@ -8,6 +8,7 @@ using System.CodeDom.Compiler;
 using System.Windows.Forms;
 using OpenQA.Selenium.DevTools.V120.Debugger;
 using static System.Runtime.InteropServices.JavaScript.JSType;
+using SeleniumExtras.WaitHelpers; // 삭제할지 고려해보기 ( alert 창 나올때까지 기다릴 수 있는 라이브러리)
 
 //담주화요일 9시30분에 최종테스트
 
@@ -30,7 +31,7 @@ namespace WebContrall_Selenium
             setStatusLabe("실행 전");
         }
 
-        private void LoginState(IWebDriver driver, IJavaScriptExecutor jsExecutor)
+        private void LoginAndGoToReservationAndReady(IWebDriver driver, IJavaScriptExecutor jsExecutor)
         {
             //자바스크립트 요소를 통해서 요소가 생성되기 전에도 클릭가능
 
@@ -44,13 +45,10 @@ namespace WebContrall_Selenium
 
 
             jsExecutor.ExecuteScript("fnLogin(1);");
-        }
 
-        private void GoToReservationAndSelectMonthDateTime(IWebDriver driver, IJavaScriptExecutor jsExecutor)
-        { 
             //예약 페이지로 이동
             driver.Navigate().GoToUrl("https://www.elysian.co.kr/reservation/golf.asp");
-            
+           
             while (true)
             { 
                 try // 10초를 기다려도 페이지가 동작 안할경우 다시 페이지 불러옴
@@ -67,6 +65,13 @@ namespace WebContrall_Selenium
                 }
             }
 
+            // 개인정보 수집 및 이용 동의 체크 미리해두기
+            jsExecutor.ExecuteScript("document.getElementById('golfAgreeY').checked = true;"); 
+
+        }
+
+        private void SelectTimeAndExecuteBooking(IWebDriver driver, IJavaScriptExecutor jsExecutor, int browserNumber)
+        {
             //부킹 목적 날자에서 날자 (예시로 2024-03-24 형태) 만 추출함
             string purposeYearMonthDay = dateTimePicker1.Value.ToString().Substring(0, 10);
             /*주석 : YearMonthDayTag 
@@ -75,14 +80,14 @@ namespace WebContrall_Selenium
             POSS 는 possible 이라는 뜻이었음 날자가 지나면 'END' 예약이 다 된 날짜는 'IMPOSS'
             XPath 의 contains 문법을 사용하여서 목적 날자와 (ex '2024-03-24' 식의형태) 일치하는 태그 찾기
             */
-            IWebElement YearMonthDayTag = null;
+            IWebElement DayTag = null;
 
-            while (true) //현재 페이지에 예약해야하는 월이 존재하지 않는경우 다음 월로 넘어감
+            while (true) //현재 페이지에 예약해야하는 ‘년－월－일’ 이 존재하지 않는경우 다음 월로 넘어감
             {
-                try 
+                try
                 {
-                    YearMonthDayTag = driver.FindElement(By.XPath("//a[contains(@onclick, '" + purposeYearMonthDay + "')]"));
-                    if (YearMonthDayTag != null) break;
+                    DayTag = driver.FindElement(By.XPath("//a[contains(@onclick, '" + DayTag + "')]"));
+                    if (DayTag != null) break;
                 }
                 catch (NoSuchElementException)
                 {
@@ -90,43 +95,52 @@ namespace WebContrall_Selenium
                 }
             }
 
-            //함수를 HTML에 나와있는 이름 그대로 하면 제대로 동작 안하고 이렇게 정확하게 XPath 로 해당 태그에 접근해서 onclick 의 함수를 호출해야동작함
-            // 이부분에 계속해서 새로고침 하면서
-            // fnChoiceDate(this, '2024-03-24', 'IMPOSS'); -> fnChoiceDate(this, '2024-03-24', 'POSS'); 로바뀔때까지 대기해야함
-            // 새로고침 태그 : <li><a href="javascript:fnCourseTimeReset();" class="reflash_btn">새로고침</a></li> 
-            jsExecutor.ExecuteScript("arguments[0].onclick()", YearMonthDayTag); 
-
-            /*주석 : HourMinuteTagsCollection
-            해당 HTML 태그
-            < a href = "javascript:void(0);" onclick = "fnChoiceCourseTime(this, 'A', 'VALLEY', '0632');" class="timeBtn golfResvCourseTime">06:32</a>
-            'fnChoiceCourseTime(this,' 가 onclick 에 포함되어있는 a 태그를 찾는다 
-            그것들을 Collection에 추가한다.
-            */
-            IReadOnlyCollection<IWebElement> HourMinuteTagsCollection = driver.FindElements(By.XPath("//a[contains(@onclick, 'fnChoiceCourseTime(this,')]"));
-
-            foreach (var HourMinuteTag in HourMinuteTagsCollection)
-            {
-                //onClickVaue 는 onclick 시 호출되는자바스크립트 함수를 문자열로 나타낸것이다.
-                string onClickValue = HourMinuteTag.GetAttribute("onclick");
-
-                //숫자부분은 항상 끝에서 7번째 에서부터 4글자이므로 이를 이용한다.
-                int startPoint = onClickValue.Length - 7;
-                int purposeHourMinute = dateTimePicker1.Value.Hour * 100 + dateTimePicker1.Value.Minute;
-
-                if (int.Parse(onClickValue.Substring(startPoint, 4)) >= purposeHourMinute)
+            while (true)
+            { 
+                try
                 {
-                    jsExecutor.ExecuteScript("arguments[0].onclick()", HourMinuteTag);
-                    return;
+                    //함수를 HTML에 나와있는 이름 그대로 하면 제대로 동작 안하고 이렇게 정확하게 XPath 로 해당 태그에 접근해서 onclick 의 함수를 호출해야동작함
+                    // 이부분에 계속해서 새로고침 하면서
+                    // fnChoiceDate(this, '2024-03-24', 'IMPOSS'); -> fnChoiceDate(this, '2024-03-24', 'POSS'); 로바뀔때까지 대기해야함
+                    // 새로고침 태그 : <li><a href="javascript:fnCourseTimeReset();" class="reflash_btn">새로고침</a></li> 
+                    jsExecutor.ExecuteScript("arguments[0].onclick()", DayTag);
+
+                    /*주석 : HourMinuteTagsCollection
+                    해당 HTML 태그
+                    < a href = "javascript:void(0);" onclick = "fnChoiceCourseTime(this, 'A', 'VALLEY', '0632');" class="timeBtn golfResvCourseTime">06:32</a>
+                    'fnChoiceCourseTime(this,' 가 onclick 에 포함되어있는 a 태그를 찾는다 
+                    그것들을 Collection에 추가한다.
+                    */
+                    IReadOnlyCollection<IWebElement> HourMinuteTagsCollection = driver.FindElements(By.XPath("//a[contains(@onclick, 'fnChoiceCourseTime(this,')]"));
+
+                    foreach (var HourMinuteTag in HourMinuteTagsCollection)
+                    {
+                        //onClickVaue 는 onclick 시 호출되는자바스크립트 함수를 문자열로 나타낸것이다.
+                        string onClickValue = HourMinuteTag.GetAttribute("onclick");
+
+                        //숫자부분은 항상 끝에서 7번째 에서부터 4글자이므로 이를 이용한다.
+                        int startPoint = onClickValue.Length - 7;
+                        int purposeHourMinute = dateTimePicker1.Value.Hour * 100 + dateTimePicker1.Value.Minute;
+
+                        if (int.Parse(onClickValue.Substring(startPoint, 4)) >= purposeHourMinute)
+                        {
+                            jsExecutor.ExecuteScript("arguments[0].onclick()", HourMinuteTag);
+                            return;
+                        }
+                    }
+
+                    //jsExecutor.ExecuteScript("fnReservation()"); // 테스트할때는 실제 등록되는걸 막기위해 주석을 할것 //순서 확인할것
+                    CancleBooking(driver);
+                }
+                catch (UnhandledAlertException)
+                {
+                    
+                    // Alert 처리
+                    IAlert alert = driver.SwitchTo().Alert();
+                    string alertText = alert.Text; // alert 창의 텍스트를 읽음
+                    alert.Accept(); // alert 창을 닫음
                 }
             }
-
-            CancleBooking(driver);
-        }
-
-        private void ExecuteBooking(IWebDriver driver, IJavaScriptExecutor jsExecutor)
-        {
-            jsExecutor.ExecuteScript("document.getElementById('golfAgreeY').checked = true;");
-            //jsExecutor.ExecuteScript("fnReservation()"); // 테스트할때는 실제 등록되는걸 막기위해 주석을 할것
         }
 
         private void CancleBooking(IWebDriver driver)
@@ -137,41 +151,37 @@ namespace WebContrall_Selenium
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //chrome 브라우저와 소통 하게 해주는 ChromeDriver
-            IWebDriver driver = new ChromeDriver();
-            /* 주석 : `IJavaScriptExecutor` 인터페이스를 사용하여 Selenium WebDriver에서 JavaScript 코드를 실행할 수 있습니다.
-             이 인터페이스는 테스트 자동화 중 웹 페이지의 자바스크립트 환경에 직접 접근하여 코드를 실행하게 해줍니다.
-             예를 들어, 페이지에 보이지 않는 요소를 클릭하거나, 페이지의 DOM을 직접 조작하는 등의 작업을 할 수 있습니다.
-             이를 통해, 테스트 시나리오에서 요구하는 복잡한 웹 상호작용을 구현할 수 있습니다. 
-            */
-            IJavaScriptExecutor jsExecutor = (IJavaScriptExecutor)driver;
+            IWebDriver driver = null;
+            IJavaScriptExecutor jsExecutor = null;
 
-            int browserNumber = BrowserNumber++; // 브라우저에 배정된 번호
-            TotalBrowservolumeLable.Text =  "현재 실행중인 브라우저 수 : " + (++TotalBrowservolume).ToString();
-
-            setStatusLabe("로그인중", browserNumber);
-            LoginState(driver, jsExecutor);
-
-            try
-            {
-                setStatusLabe("부킹 페이지로 이동 후 날자 및 시간 선택중", browserNumber);
-                GoToReservationAndSelectMonthDateTime(driver, jsExecutor);
-            }
-            catch(UnhandledAlertException)
-            {
-                CancleBooking(driver);
-                setStatusLabe(" 종료됨 : 날자가 올바르게 입력되었는지 다시 확인해주세요", browserNumber);
-                return;
-            }
-
-            try
-            {
-                setStatusLabe("날자, 시간 확인후 실제 부킹 등록중", browserNumber);
-                ExecuteBooking(driver, jsExecutor);
-            }
-            catch (WebDriverException) 
+            while (true)
             { 
-                setStatusLabe("해당 날자 예약 가능한 시간 없어 종료", browserNumber);
+                try
+                {
+                    //chrome 브라우저와 소통 하게 해주는 ChromeDriver
+                    driver = new ChromeDriver();
+                    
+                    /* 주석 : `IJavaScriptExecutor` 인터페이스를 사용하여 Selenium WebDriver에서 JavaScript 코드를 실행할 수 있습니다.
+                     이 인터페이스는 테스트 자동화 중 웹 페이지의 자바스크립트 환경에 직접 접근하여 코드를 실행하게 해줍니다.
+                     예를 들어, 페이지에 보이지 않는 요소를 클릭하거나, 페이지의 DOM을 직접 조작하는 등의 작업을 할 수 있습니다.
+                     이를 통해, 테스트 시나리오에서 요구하는 복잡한 웹 상호작용을 구현할 수 있습니다. 
+                    */
+                    jsExecutor = (IJavaScriptExecutor)driver;
+
+                    int browserNumber = BrowserNumber++; // 브라우저에 배정된 번호
+                    TotalBrowservolumeLable.Text = "현재 실행중인 브라우저 수 : " + (++TotalBrowservolume).ToString();
+
+                    setStatusLabe("로그인 및 예약 페이지로 이동중", browserNumber);
+                    LoginAndGoToReservationAndReady(driver, jsExecutor);
+
+                    setStatusLabe("날자 및 시간 선택후 등록중", browserNumber);
+
+                    SelectTimeAndExecuteBooking(driver, jsExecutor, browserNumber);
+                }
+                catch (JavaScriptException)
+                {
+                    CancleBooking(driver);
+                }
             }
         }
 
